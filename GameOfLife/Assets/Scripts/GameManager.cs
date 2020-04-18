2a5d4e027e68a23d579e267fd67843db1d6c018a
dweb-damienweber <60881591+dweb-damienweber@users.noreply.github.com>
2020-04-19 00:27:29 +0200

    Version 1

---
+++ GameOfLife/Assets/Scripts/GameManager.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region System methods
+
+    private void Awake()
+    {
+        _cameraTransform = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+
+        if (!_tilesContainer || !_tilePrefab || !_cameraTransform)
+            Debug.LogError("Missing references");
+
+        GenerateBoard();
+        _nextTurn = Time.time + _timeBeforeNextTurn;
+    }
+
+    private void Update()
+    {
+        if (Time.time > _nextTurn)
+        {
+            CheckTiles();
+            _nextTurn = Time.time + _timeBeforeNextTurn;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            GenerateBoard();
+    }
+
+    #endregion
+
+
+    #region Methods
+
+    /// <summary>
+    /// Create the initial board
+    /// </summary>
+    private void GenerateBoard()
+    {
+        _tiles = new Tile[_width, _height];
+
+        for (int h = 0; h < _height; h++)
+        {
+            for (int w = 0; w < _width; w++)
+            {
+                Vector2 position = new Vector2(w, h);
+                Tile tile = Instantiate(_tilePrefab, position, Quaternion.identity, _tilesContainer);
+                tile.gameObject.name = $"Tile_{w}_{h}";
+                SetIfTileIsAlive(tile);
+                _tiles[w, h] = tile;
+            }
+        }
+
+        SetCameraPosition();
+    }
+
+    /// <summary>
+    /// Center camera on the map
+    /// </summary>
+    private void SetCameraPosition()
+    {
+        Vector3 position = new Vector3(_width * .5f - .5f, _height * .5f - .5f, -10);
+        _cameraTransform.position = position;
+    }
+
+    /// <summary>
+    /// Is the tile alive ? (Used when the board is generated)
+    /// </summary>
+    private void SetIfTileIsAlive(Tile tile)
+    {
+        if (Random.Range(0, 6) == 1)
+            tile.ChangeState(true);
+        else
+            tile.ChangeState(false);
+    }
+
+    /// <summary>
+    /// Check if the tiles are alive or not
+    /// </summary>
+    private void CheckTiles()
+    {
+        for (int h = 0; h < _height; h++)
+        {
+            for (int w = 0; w < _width; w++)
+            {
+                int n = HowManyAliveAround(w, h);
+
+                if (_tiles[w, h].IsAlive())
+                {
+                    if (n < 2 || n > 3)
+                        _tiles[w, h].ChangeState(false);
+                    else if (n == 2 || n == 3)
+                        _tiles[w, h].ChangeState(true);
+                }
+                else
+                {
+                    if (n == 3)
+                        _tiles[w, h].ChangeState(true);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Return how many living tile is around the given tile
+    /// </summary>
+    private int HowManyAliveAround(int w, int h)
+    {
+        int n = 0;
+
+        if (w > 0 && _tiles[w - 1, h].IsAlive())
+            n++;
+        if (w < _width - 1 && _tiles[w + 1, h].IsAlive())
+            n++;
+        if (h > 0 && _tiles[w, h - 1].IsAlive())
+            n++;
+        if (h < _height - 1 && _tiles[w, h + 1].IsAlive())
+            n++;
+
+        if (w > 0 && h < _height - 1 && _tiles[w - 1, h + 1].IsAlive())
+            n++;
+        if (w < _width - 1 && h < _height - 1 && _tiles[w + 1, h + 1].IsAlive())
+            n++;
+        if (w > 0 && h > 0 && _tiles[w - 1, h - 1].IsAlive())
+            n++;
+        if (w < _width - 1 && h > 0 && _tiles[w + 1, h - 1].IsAlive())
+            n++;
+
+        return n;
+    }
+
+    #endregion
+
+
+    #region Private fields
+
+    private Tile[,] _tiles;
+
+    [Header("Settings")]
+
+    [SerializeField] private int _width = 35;
+    [SerializeField] private int _height = 20;
+    [SerializeField] private float _timeBeforeNextTurn = 1f;
+
+    [Header("References")]
+
+    [SerializeField] private Transform _tilesContainer = null;
+    [SerializeField] private Tile _tilePrefab = null;
+    private Transform _cameraTransform;
+
+    private float _nextTurn;
+
+    #endregion
+}
---
+++ GameOfLife/Assets/Scripts/Tile.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    #region System methods
+
+    private void Awake()
+    {
+        _isAlive = false;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    #endregion
+
+
+    #region Methods
+
+    public bool IsAlive() => _isAlive;
+
+    public void ChangeState(bool state)
+    {
+        _isAlive = state;
+
+        if (_isAlive)
+            _spriteRenderer.color = _aliveColor;
+        else
+            _spriteRenderer.color = _deadColor;
+    }
+
+    #endregion
+    
+    
+    #region Private fields
+
+    private bool _isAlive;
+    private SpriteRenderer _spriteRenderer;
+
+    [Header("Colors")]
+
+    [SerializeField] private Color _deadColor = new Color();
+    [SerializeField] private Color _aliveColor = new Color();
+
+    #endregion
+}
